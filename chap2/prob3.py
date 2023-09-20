3cf56409643f2fa77aeed66310d89ba41da381d6
LMH <vpsxja3268@cu.ac.kr>
2023-09-20 02:56:17 +0000

    prod3-no perpect

---
+++ chap2/prob3.py
@@ -0,0 +1,8 @@
+Name = int(print('Hi, What\'s your name? '))
+year = int(print('And how old are you? '))
+weigh = int(print('Okay, last question. How many pounds do you weigh? '))
+
+print('')
+print('Did you know that your\'re just 4 in dog year?', year/4)
+print('')
+

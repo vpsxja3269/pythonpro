02a55fbf01e9101be83fbe50a943776cf659c964
LMH <vpsxja3268@cu.ac.kr>
2023-10-04 02:22:43 +0000

    test

---
+++ chap5/prob2.py
@@ -0,0 +1,22 @@
+import random
+
+words = ("difficult", "banana", "apple", "python", "daegu", "catholic", "university")
+
+print("Welcome to word Jumble!\n Unscramble the letters to make a word.")
+
+Jumble = random.choice(words)
+x = list(Jumble)
+random.shuffle(x)
+
+print("Jumble word : ",end='')
+for i in range(len(x)):
+    print(x[i],end='')
+
+print("\n")
+
+guess = input("Your guess : ")
+if Jumble == guess:
+    print("good Man~")
+else:
+    print("Sorry, that's not correct. The word was: apple")
+

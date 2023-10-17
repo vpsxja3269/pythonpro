0f1f283695467d03b9dec98b32d7b95626c29a1c
vpsxja3269 <vpsxja3268@naver.com>
2023-10-17 14:36:54 +0900

    chap2 prob1, prob2 modify

--- chap2/prob2.py
+++ chap2/prob2.py
@@ -1,10 +1,12 @@
 print('\t\t\tTrust Fund Buddy')
+
 print('Totals your monthly spending so that your trust found doesn\'t run out')
 print('<and your\'re forced to get a real job>')
 print('\n')
 print('Please onter the requseted, monthly costs.   Since your\'re rich. ignore pannies')
 print('and use only dollar amounts.')
 print('\n')
+
 Tune = int(input("lamborghini Tune-Ups : "))
 Apt = int(input("Manhattan Apartment : "))
 Jet = int(input("Private Jet Rental : "))
@@ -13,6 +15,7 @@ Dinning = int(input("Dinning Out : "))
 Staff = int(input("Staff <Butlers, chef, driver, assistant> : "))
 Guru = int(input("Persinal Guru and Coach : "))
 Game = int(input("Computer Games : "))
+
 total = Tune + Apt + Jet + Gif + Dinning + Staff + Guru + Game
 print("Grand Total :", total)
 
--- chap2/prob3.py
+++ chap2/prob3.py
@@ -1,8 +1,18 @@
-Name = int(print('Hi, What\'s your name? '))
-year = int(print('And how old are you? '))
-weigh = int(print('Okay, last question. How many pounds do you weigh? '))
+Name = input("What's your name? ")
+Age = int(input("And how old are you? "))
+Weigh = int(input("Okay, last question. How many pounds do you weigh? "))
 
-print('')
-print('Did you know that your\'re just 4 in dog year?', year/4)
-print('')
+age_dog = Age/7
+age_seconds = Age * 31536000
 
+print("Did you know that you're just",age_dog, "in dog years?")
+print("\nBut you're also over", age_seconds, "seconds old.")
+
+print("\nIf a small child were trying to get your attention, your name would become ", Name, Name, Name, Name, Name, sep="")
+
+moon_weigh = Weigh / 6
+sun_weigh = Weigh * 27.1
+
+print("\nDid you know that on the moon you would weigh only", moon_weigh, "pounds?")
+print("But on the sun, you'd weigh", sun_weigh, "<but, ah... not for long>.")
+print("\nPress the enter key to exit.")

b56f9595d74b1a86c1099585474c50065a7f1f35
LMH <vpsxja3268@cu.ac.kr>
2023-10-24 23:06:13 +0900

    Tic Tac Toe game day.1

---
+++ chap7/prob1_TTT.py
@@ -0,0 +1,16 @@
+import random
+
+def intro(){
+    print("]n")
+    print("\tWelcome to the greatest intellectual challenge of all time: Tic-Tac-Toe.")
+    print("\tThis will be a showdown between your human brain and my silicon processor.\n\n")
+    print("\tYou will make your move known by entering a number, 0 - 8.")
+    print("\tThe number will correspond to the board position as illustrated:")
+    print("\t\t\t\t0 | 1 | 2")
+    print("\t\t\t\t---------")
+    print("\t\t\t\t3 | 4 | 5")
+    print("\t\t\t\t---------")
+    print("\t\t\t\t6 | 7 | 8")
+    print("\n\tPrepare yourself, human. The ultimate battle is about to begin.")
+    print("\n\n")
+}

0a3ea36d12660d885182716a9391ef3d865f92c9
LMH <vpsxja3268@cu.ac.kr>
2023-12-12 22:26:03 +0900

    prob3

---
+++ chap9/prob3/main.py
@@ -0,0 +1,18 @@
+class Critter:
+    def __init__(self, name):
+        print("A new critter has been born!")
+        self.name = name
+        print("Hi. I'm", self.name, "\n")
+
+crit1 = Critter("Poochie")
+
+class Critter:
+    def __init__(self, name):
+        print("A new critter has been born!")
+        self.name = name
+        print("Hi. I'm", self.name, "\n")
+
+crit2 = Critter("Randolph")
+
+n = input('Press the enter key to exit.')
+#27 36 44
